6f032963725ff8a8669264245922c9a6f1f18cc5
Mitch <wabbitsnot@gmail.com>
2013-10-21 16:34:49 -0700

    first commit / rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitchipedia::Application.config.secret_key_base = 'cd4432945519b193c04b36e5ce5f3197af330a2238eafff87c9d7018005454d0fdcef7b1286faf5dd58480ca562e51a63d8251346792580ae995780eec50619b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mitchipedia::Application.config.session_store :cookie_store, key: '_mitchipedia_session'

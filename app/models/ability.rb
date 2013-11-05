88b00491831a5e25ef05ded539afe29ac31171cb
Mitch <wabbitsnot@gmail.com>
2013-11-04 21:31:15 -0800

    minor changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,22 +5,13 @@ class Ability
     # guest - someone who is not logged in. can only see public wikis and public pages
     user ||= User.new
 
+    alias_action :create, :read, :edit, to: :access_level_2_actions
+
     # member - can create/manage their own wikis and pages
     if user.role? :member
       can :manage, Wiki, owner_id: user.id
-      user.collaborators.each do |c|
-        if c.access_level == 2
-          can :edit, Wiki, id: c.wiki_id
-          can :read, Wiki, id: c.wiki_id
-          can :create, Wiki, id: c.wiki_id
-          return
-        elsif c.access_level == 1
-          can :read, Wiki, id: c.wiki_id
-          cannot :edit, Wiki, id: c.wiki_id
-          can :create, Wiki, id: c.wiki_id
-          return
-        end
-      end
+      access_level_2_wikis = user.collaborators.select {|c| c.access_level == 2}.collect(&:wiki_id) 
+      can :access_level_2_actions, Wiki, id: access_level_2_wikis
     elsif user.role? :admin
       can :manage, :all
     else
